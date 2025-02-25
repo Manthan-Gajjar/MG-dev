af7a65393d45ec8894084a06f0d41d0ed95c67fc
Manthan-Gajjar <mgdev.coder@gmail.com>
2025-02-25 23:37:31 +0530

    Add Cache-Control headers to next.config for improved caching strategy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["nudefnihnnkkjaivntjr.supabase.co"], // Replace with your actual Supabase domain
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["nudefnihnnkkjaivntjr.supabase.co"], // Replace with your actual Supabase domain
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
